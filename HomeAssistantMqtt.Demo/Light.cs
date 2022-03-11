1f97dbe4fa181df7820f0b392b221329c7c10997
Matteo Garia <m.garia@maticafintec.com>
2022-03-11 16:37:16 +0100

    Added (untested) sample

---
+++ HomeAssistantMqtt.Demo/Light.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Newtonsoft.Json;
+using HomeAssistantMqtt.Models;
+
+namespace HomeAssistantMqtt.Demo
+{
+    public class LightMessage
+    {
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("brightness")]
+        public int? Brightness { get; set; }
+
+        [JsonIgnore]
+        public bool On { get => State.ToLower() == "on"; set => State = value ? "ON" : "OFF"; }
+    }
+
+    public class Light : IDevice<LightMessage>
+    {
+        public Action<LightMessage> StatusNotification { get; set; }
+
+        public void CommandNotification(LightMessage command)
+        {
+            Console.WriteLine($"Received Command status: {command?.State} brightness: {command?.Brightness}");
+            StatusNotification?.Invoke(command);
+        }
+
+        // nothing to customize here
+        public DiscoveryMessage GetDiscoveryMessage()
+            => new DiscoveryMessage();
+
+        public LightMessage GetStatus()
+            => new LightMessage() { On = false, Brightness = 0 };
+
+        public void MessageNotification(string topic, string msg)
+             => Console.WriteLine($"Received from {topic} msg: {msg}");
+    }
+}
---
+++ HomeAssistantMqtt.Demo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using MQTTnet.Extensions.ManagedClient;
+using HomeAssistantMqtt.Models;
+
+namespace HomeAssistantMqtt.Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sample broker configuration
+            MqttBrokerConfiguration broker = new MqttBrokerConfiguration
+            {
+                ClientId = "sampleClient",
+                IpAddress = "192.168.1.100",
+                Username = "sampleUser",
+                Password = "samplePassword"
+            };
+
+            // Sample device
+            MqttDeviceDescriptor deviceDescriptor = new MqttDeviceDescriptor
+            {
+                Id = "ha-mqtt",
+                Name = "HomeAssistantMQTT",
+                Manufactorer = "SomeManufactorer",
+                Model = "SomeModel",
+                SoftwareVersion = "8.4.7.2"
+            };
+
+            // Sample light
+            MqttEntityDescriptor lightDescriptor = new MqttEntityDescriptor
+            {
+                ComponentClass = "light",
+                EntityId = "samplelight",
+                Name = "MQTT Sample Light",
+                Publish = true,
+                Route = "sample/light",
+                UpdateOnStart = true,
+            };
+
+            ManagedMqttClient mqttClient = MqttDevice.CreateMqttClient(broker);
+            MqttDevice mqttDevice = new MqttDevice(mqttClient, deviceDescriptor);
+
+            MqttEntity<LightMessage> sampleLightEntity = new MqttEntity<LightMessage>(new Light(), lightDescriptor, mqttDevice);
+
+
+            Console.ReadLine();
+        }
+    }
+}
